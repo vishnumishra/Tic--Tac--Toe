b82a50b933998325e791e7ccea819536a84a88d0
vishnumishra <vishnu143mishra@gmail.com>
2015-03-26 16:34:04 +0530

    Tic tac toe game is finished refactoring is remain

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -10,30 +10,66 @@ public class Game {
     public void initBoard() {
         gameBoard.put(1, "- ");
         gameBoard.put(2, "- ");
-        gameBoard.put(3, "-");
+        gameBoard.put(3, "- ");
         gameBoard.put(4, "- ");
         gameBoard.put(5, "- ");
-        gameBoard.put(6, "-");
+        gameBoard.put(6, "- ");
         gameBoard.put(7, "- ");
         gameBoard.put(8, "- ");
-        gameBoard.put(9, "-");
+        gameBoard.put(9, "- ");
+    }
+
+
+    public boolean isPlayerWin(String playerSymbol) {
+        int[] diagonal1={1,5,9};
+        int[] diagonal2={3,5,7};
+        int[] r1={1,2,3};
+        int[] r2={4,5,6};
+        int[] r3={7,8,9};
+        int[] c1={1,4,7};
+        int[] c2={2,5,8};
+        int[] c3={3,6,9};
+
+        int[][] allConditions = {diagonal1,diagonal2,r1,r2,r3,c1,c2,c3};
+        boolean win = true;
+        for (int[] condition : allConditions) {
+            for (int i : condition) {
+                if(!gameBoard.get(i).equals(playerSymbol))
+                    win = false;
+            }
+            if(win) return true;
+            win = true;
+        }
+        return false;
     }
 
     public void play(){
-        Integer count=0;
-        String player1 = "X ";
-        String player2 = "O ";
+        Integer count=1;
+        String playerSymbol="";
         initBoard();
-        while (++count <=9){
+        while (count <=9){
             System.out.println("please enter the choice...");
+            System.out.println("Play user "+getPlayerSymbol(count)+"\n");
             Scanner sc = new Scanner(System.in);
             int userChoice = sc.nextInt();
-            if(count % 2 ==0)
-               gameBoard.put(userChoice,player1);
-            else gameBoard.put(userChoice,player2);
-            System.out.println(GameBoard.getBoard(getBoard()));
+            if(gameBoard.get(userChoice).equals("- ")){
+                playerSymbol =getPlayerSymbol(count);
+                gameBoard.put(userChoice,playerSymbol);
+                count++;
+            }
 
+            if(isPlayerWin(playerSymbol)){
+                System.out.println("**************  Player " + playerSymbol + " Win ********************\n");
+                System.out.println(GameBoard.getBoard(getBoard()));
+                System.exit(0);
+            }
+            System.out.println(GameBoard.getBoard(getBoard()));
         }
+        System.out.println("..............Game Dra .............");
+    }
+
+    private String getPlayerSymbol(Integer count) {
+        return (count % 2 == 0)?"O ":"X ";
     }
 
     public  Map<Integer, String> getBoard() {
--- test/GameTest.java
+++ test/GameTest.java
@@ -1,10 +1,12 @@
 import com.company.Game;
+import com.company.GameBoard;
 import org.junit.Test;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -24,10 +26,19 @@ public class GameTest {
     @Test
     public void testInitBoardShouldCreateTheBoard() throws IOException {
         Game game = new Game();
-//        Map<Integer, String> board = game.getBoard();
-//        System.out.println(GameBoard.getBoard(board));
-//        equalsFile("createGameBoard","emptyBoard", GameBoard.getBoard(game.initBoard()));
+        Map<Integer, String> board = game.getBoard();
+        System.out.println(GameBoard.getBoard(board));
         game.play();
+    }
+
+    @Test
+    public void isPlayerWinShouldGiveTrueIfDiagnolsAreSame(){
+        Game game = new Game();
+        game.initBoard();
+        game.gameBoard.put(1,"X ");
+        game.gameBoard.put(4,"X ");
+        game.gameBoard.put(7,"X ");
+        assertEquals(game.isPlayerWin("X "),true);
     }
 
 
